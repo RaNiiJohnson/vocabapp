b18a80fddc6db93e780cd74d672658239e6453ea
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2024-05-09 07:55:56 +0300

    header

--- src/features/layout/Header.tsx
+++ src/features/layout/Header.tsx
@@ -2,8 +2,8 @@ import { ThemeToggle } from "@/theme/ThemeToggle";
 
 export const Header = () => {
   return (
-    <div className="flex items-center gap-4 py-2 border-b-2 sticky top-0 z-10 backdrop-blur-sm">
-      <h1 className="flex-1 text-2xl font-bold ">Vocabulary Note</h1>
+    <div className="sticky top-0 z-10 flex items-center max-w-screen-xl gap-4 py-2 m-auto border-b-2 max-lg:px-2 md:container backdrop-blur-sm">
+      <h1 className="flex-1 text-2xl font-bold">Vocabulary Note</h1>
       <ThemeToggle />
     </div>
   );
