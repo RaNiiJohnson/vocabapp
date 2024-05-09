5844e920bde8e9b41b02a3e3b8d8ae5312448a9d
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2024-05-09 07:50:50 +0300

    add idiom section

--- app/(vocabulary)/page.tsx
+++ app/(vocabulary)/page.tsx
@@ -1,8 +1,18 @@
 import { prisma } from "@/lib/prisma";
 import { VocabItem } from "./VocabItem";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { buttonVariants } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { cn } from "@/lib/utils";
+import { StarFilledIcon } from "@radix-ui/react-icons";
+import clsx from "clsx";
 import { VocabForm } from "./VocabForm";
 
 export default async function Home() {
@@ -15,13 +25,40 @@ export default async function Home() {
   return (
     <div className="space-y-4">
       <VocabForm />
-      <Tabs defaultValue="tout" className="">
-        <TabsList className="grid w-full grid-cols-5">
-          <TabsTrigger value="tout">Tout</TabsTrigger>
-          <TabsTrigger value="verbe">Verbe</TabsTrigger>
-          <TabsTrigger value="adjectif">Adjectif</TabsTrigger>
-          <TabsTrigger value="nom">Nom</TabsTrigger>
-          <TabsTrigger value="autres">Autres</TabsTrigger>
+      <Tabs defaultValue="tout" className="relative">
+        <TabsList className="grid w-full grid-cols-6 ">
+          <TabsTrigger className="max-sm:text-xs" value="tout">
+            Tout
+          </TabsTrigger>
+          <TabsTrigger className="max-sm:text-xs" value="verbe">
+            Verbes
+          </TabsTrigger>
+          <TabsTrigger className="max-sm:text-xs" value="adjectif">
+            Adjectifs
+          </TabsTrigger>
+          <TabsTrigger className="max-sm:text-xs" value="nom">
+            Noms
+          </TabsTrigger>
+          <TabsTrigger className="max-sm:text-xs" value="idiomatique">
+            Idiomatiques
+          </TabsTrigger>
+          <TabsTrigger className="max-sm:text-xs" value="autres">
+            Autres
+          </TabsTrigger>
+          <TabsTrigger
+            className={clsx(
+              "absolute top-10 right-2 max-sm:text-xs",
+              buttonVariants({ variant: "secondary" })
+            )}
+            value="favoris"
+          >
+            <StarFilledIcon
+              className={cn("text-yellow-500 size-6 dark:text-yellow-400", {
+                "text-secondary-foreground dark:text-secondary-foreground":
+                  vocabularies.filter((vocab) => vocab.favorite).length === 0,
+              })}
+            />
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="tout">
           <Card>
@@ -33,7 +70,9 @@ export default async function Home() {
             <CardContent className="space-y-2">
               <ul className="space-y-3">
                 {vocabularies.length === 0 ? (
-                  <p>Pas de vocabulaire pour le moment</p>
+                  <p className="text-secondary-foreground/45">
+                    Pas de vocabulaire pour le moment
+                  </p>
                 ) : (
                   vocabularies.map((vocabulary) => (
                     <li key={vocabulary.id}>
@@ -54,7 +93,8 @@ export default async function Home() {
             </CardHeader>
             <CardContent className="space-y-2">
               <ul className="space-y-3">
-                {vocabularies.length === 0 ? (
+                {vocabularies.filter((vocab) => vocab.type === "verbe")
+                  .length === 0 ? (
                   <p>Pas de vocabulaire pour le moment</p>
                 ) : (
                   vocabularies
@@ -78,7 +118,8 @@ export default async function Home() {
             </CardHeader>
             <CardContent className="space-y-2">
               <ul className="space-y-3">
-                {vocabularies.length === 0 ? (
+                {vocabularies.filter((vocab) => vocab.type === "adjectif")
+                  .length === 0 ? (
                   <p>Pas de vocabulaire pour le moment</p>
                 ) : (
                   vocabularies
@@ -96,17 +137,72 @@ export default async function Home() {
         <TabsContent value="nom">
           <Card>
             <CardHeader>
+              {" "}
               <CardTitle>
-                <span className="text-xl">Nom</span>
+                <span className="text-xl">Noms</span>
               </CardTitle>
             </CardHeader>
             <CardContent className="space-y-2">
               <ul className="space-y-3">
-                {vocabularies.length === 0 ? (
+                {vocabularies.filter((vocab) => vocab.type === "nom").length ===
+                0 ? (
                   <p>Pas de vocabulaire pour le moment</p>
                 ) : (
                   vocabularies
                     .filter((vocab) => vocab.type === "nom")
+                    .map((vocabulary) => (
+                      <li key={vocabulary.id}>
+                        <VocabItem vocab={vocabulary} />
+                      </li>
+                    ))
+                )}
+              </ul>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="idiomatique">
+          <Card>
+            <CardHeader>
+              {" "}
+              <CardTitle>
+                <span className="text-xl">Idiomatiques</span>
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <ul className="space-y-3">
+                {vocabularies.filter((vocab) => vocab.type === "idiomatique")
+                  .length === 0 ? (
+                  <p>Pas d{"'"}expression Idiomatique pour le moment</p>
+                ) : (
+                  vocabularies
+                    .filter((vocab) => vocab.type === "idiomatique")
+                    .map((vocabulary) => (
+                      <li key={vocabulary.id}>
+                        <VocabItem vocab={vocabulary} />
+                      </li>
+                    ))
+                )}
+              </ul>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="favoris">
+          <Card>
+            <CardHeader>
+              <CardTitle>
+                <span className="text-xl">Favoris</span>
+              </CardTitle>
+              <CardDescription>Vos favoris.</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <ul className="space-y-3">
+                {vocabularies.filter((vocab) => vocab.favorite).length === 0 ? (
+                  <p className="text-secondary-foreground/45">
+                    Pas de vocabulaire favoris pour le moment
+                  </p>
+                ) : (
+                  vocabularies
+                    .filter((vocab) => vocab.favorite)
                     .map((vocabulary) => (
                       <li key={vocabulary.id}>
                         <VocabItem vocab={vocabulary} />
@@ -123,10 +219,14 @@ export default async function Home() {
               <CardTitle>
                 <span className="text-xl">Autres</span>
               </CardTitle>
+              <CardDescription>
+                Adverbes, locution adverbiale, etc.
+              </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
               <ul className="space-y-3">
-                {vocabularies.length === 0 ? (
+                {vocabularies.filter((vocab) => vocab.type === "autres")
+                  .length === 0 ? (
                   <p>Pas de vocabulaire pour le moment</p>
                 ) : (
                   vocabularies
